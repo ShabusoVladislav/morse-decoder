dfd8295a76598841b335887a8115b3ad50b0d7dd
Vladislav Shabusov <vladshabusov66@gmail.com>
2021-08-21 03:12:33 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+    let signsArr = [];
+    let k = 0;  //counter for array with . and -
+    for (let i = 0; i < expr.length / 10; i++) {    //in every element of signsArr coded letter
+        let signsStr = '';
+        for (let j = 0; j < 10; j += 2) {
+            if (`${expr[10*i+j]}${expr[10*i+j+1]}` === '10') {
+                signsStr += '.';
+            } else if (`${expr[10*i+j]}${expr[10*i+j+1]}` === '11') {
+                signsStr += '-';
+            } else if (`${expr[10*i+j]}` === '*') {
+                signsStr += ' ';
+                j += 8;
+            }
+        }
+        signsArr[k++] = signsStr;
+    }
+
+    for (let i = 0; i < signsArr.length; i++) { //write to the str by letters
+        if (signsArr[i] === ' ') {  //checking space
+            str += ' ';
+            continue;
+        }
+        for (let key in MORSE_TABLE) {
+            if (key === signsArr[i]) {
+                str += MORSE_TABLE[key];
+                break;
+            }
+        }
+    }
+    return str;
 }
 
 module.exports = {
